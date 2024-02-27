721cf0e1fd5a2cd868cbe4590737849c65b2720a
Hmddev23 <hamid.maira709@gmail.com>
2024-02-26 17:05:48 -0800

    fix models files 1

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,21 @@
-#!/usr/bin/python3
-"""
-initialize the models package
-"""
+import os
+from models.base_model import BaseModel
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.user import User
 
-from os import getenv
+"""initialize the models package"""
 
-
-storage_t = getenv("HBNB_TYPE_STORAGE")
-
-if storage_t == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
+if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine import db_storage
+    CNC = db_storage.DBStorage.CNC
+    storage = db_storage.DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
+    from models.engine import file_storage
+    CNC = file_storage.FileStorage.CNC
+    storage = file_storage.FileStorage()
+
 storage.reload()
--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,20 @@
-#!/usr/bin/python
-""" holds class Amenity"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
+#!/usr/bin/python3
+"""
+hold class Amenity
+"""
+import os
+from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm import backref
+from models.base_model import BaseModel, Base
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if models.storage_t == 'db':
+    """Amenity class handles all application amenities"""
+    if storage_type == "db":
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity")
     else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+        name = ''
